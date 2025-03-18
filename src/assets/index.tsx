ab8ea6a249bf0c2f9aa62da423128f4d5dc912fc
Thermiee <navyblack16@yahoo.com>
2025-03-18 05:26:39 +0100

    add image

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -2,7 +2,7 @@ import AuthBg from "./svg/AuthBackground.svg";
 import Home from "./svg/home2.svg";
 import Person from "./svg/profile.svg";
 import Logout from "./svg/logout.svg";
-import Leads from "./svg/message-icon.svg";
+import Leads from "./svg/dashboard.svg";
 import Help from "./svg/help.svg";
 import Reports from "./svg/bookings.svg";
 
