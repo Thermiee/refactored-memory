e593994c56d092d6a2a737e548fd30c812d2fc81
Thermiee <navyblack16@yahoo.com>
2025-03-18 06:59:46 +0100

    add data to dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -4,31 +4,45 @@ import { PiBuildingApartmentFill } from "react-icons/pi";
 import { CardType } from "../../types";
 import { GrTransaction } from "react-icons/gr";
 import { FaTasks } from "react-icons/fa";
+import BarChart from "../../Components/BarChart";
 
 const Home: React.FC = () => {
-
+  const data = {
+    jan: 20000,
+    feb: 30000,
+    mar: 4000,
+    apr: 5000,
+    may: 6000,
+    june: 70000,
+    july: 80000,
+    aug: 90000,
+    sept: 10000,
+    oct: 11000,
+    nov: 100000,
+    dec: 130000,
+  };
 
   const cards: CardType[] = [
     {
       description: "Total Users",
       icon: <IoIosPeople className="text-2xl text-primary" />,
-      number: 162
+      number: 162,
     },
     {
       description: "Total Companies",
       icon: <PiBuildingApartmentFill className="text-2xl text-primary" />,
-      number: 43
+      number: 43,
     },
     {
       description: "Total Transactions",
       icon: <GrTransaction className="text-2xl text-primary" />,
-      number: 6423
+      number: 6423,
     },
     {
       description: "Total Tasks",
-      icon: <FaTasks  className="text-2xl text-primary" />,
-      number: 89
-    }
+      icon: <FaTasks className="text-2xl text-primary" />,
+      number: 89,
+    },
   ];
   return (
     <div>
@@ -43,13 +57,17 @@ const Home: React.FC = () => {
               <div className="lg:text-xl font-semibold ml-2">
                 {card.description}
               </div>
-                <div className="flex items-center">
-                {card.icon}
-                </div>
+              <div className="flex items-center">{card.icon}</div>
             </div>
             <div className="font-bold ml-3">{card.number}</div>
           </div>
         ))}
+      </div>
+      <div className="mt-5">
+        <div className="flex space-x-3 text-center items-center">
+          <p className="text-2xl font-bold">Monthly Revenue</p>
+        </div>
+        <BarChart data={data} />
       </div>
     </div>
   );
