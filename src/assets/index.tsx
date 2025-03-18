b423122e04b94d2dbacd28e3da133dca04b181d3
Thermiee <navyblack16@yahoo.com>
2025-03-18 05:24:31 +0100

    add icons

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,9 @@
-import AuthBg from './svg/AuthBackground.svg'
-import Home from "./svg/home2.svg"
+import AuthBg from "./svg/AuthBackground.svg";
+import Home from "./svg/home2.svg";
+import Person from "./svg/profile.svg";
+import Logout from "./svg/logout.svg";
+import Leads from "./svg/message-icon.svg";
+import Help from "./svg/help.svg";
+import Reports from "./svg/bookings.svg";
 
-export { AuthBg, Home }
+export { AuthBg, Home, Person, Leads, Logout, Help, Reports };
