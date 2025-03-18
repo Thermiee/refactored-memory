65371849ab5d25ab32f59bcd1e758165405e3ab4
Thermiee <navyblack16@yahoo.com>
2025-03-18 05:24:43 +0100

    add side bar

--- src/Components/Sidebar/data.ts
+++ src/Components/Sidebar/data.ts
@@ -1,20 +1,60 @@
-import { Home } from "../../assets"
+import { Help, Home, Leads, Logout, Person, Reports } from "../../assets";
 
 export interface SidebarLinks {
+  id: number;
+  title: string;
+  url: string;
+  image?: string | React.ReactElement;
+  headingText: string;
+}
 
-    id: number
-    title: string
-    url: string
-    image?: string | React.ReactElement
-    headingText: string
-  }
-
-  export const sidebarLinks: SidebarLinks[] = [
+export const crmSidebarLinks: SidebarLinks[] = [
+  {
+    id: 1,
+    title: "Dashboard",
+    url: "home",
+    image: Home,
+    headingText: "Dashboard",
+  },
     {
-      id: 1,
-      title: 'Dashboard',
-      url: 'dashboard',
-      image: Home,
-      headingText: 'Dashboard',
+        id: 2,
+        title: "Leads",
+        url: "#",
+        image: Leads,
+        headingText: "Leads",
     },
-]
+  {
+    id: 3,
+    title: "Users",
+    url: "#",
+    image: Person,
+    headingText: "Users",
+  },
+
+  {
+    id: 4,
+    title: "Tasks",
+    url: "#",
+    image: Help,
+    headingText: "Tasks",
+  },
+
+  {
+    id: 5,
+    title: "Reports",
+    url: "#",
+    image: Reports,
+    headingText: "Reports",
+  },
+
+];
+
+export const crmBottomLinks: SidebarLinks[] = [
+    {
+        id: 1,
+        title: "Logout",
+        url: "/",
+        image: Logout,
+        headingText: "Logout",
+    },
+];
